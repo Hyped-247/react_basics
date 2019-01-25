60c57e7c314f6a805c97a404e3ae1acfcefc3acd
Mohammad Mahjoub <mmahjoub@westmont.edu>
2019-01-24 17:26:18 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom'
+import React,  { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
 
-export default App;
+let appObject = {
+  title: "My new title here.",
+  subtitle: "Here is my subtitle. ",
+    options: ['one', 'two']
+};
+
+let template = (
+  <div>
+    <h1>{appObject.title}</h1>
+    {appObject.subtitle && <p>{appObject.subtitle}</p>}
+    {appObject.options.length > 0 ? "here are your options" : "No options"}
+  </div>
+);
+let appRoot = document.getElementById("root2");
+ReactDOM.render(template, appRoot);
+
+
+
+// let template = (
+//     <div>
+//       <h1>This is my html</h1>
+//       <h1>This is my html</h1>
+//       <h1>This is my html</h1>
+//       <ol>
+//         <li>item 1</li>
+//         <li>item 2</li>
+//         <li>item 3</li>
+//       </ol>
+//     </div>
+// );
+//
+//
+// let appRoot = document.getElementById("root");
+// ReactDOM.render(template, appRoot);
+
+// var username = "Mo";
+// var age = 22;
+// var location = 'New York';
+//
+// var userInformation = {
+//     name: 'Mohammad Mahjoub',
+//     age: 17,
+//     location: 'Saudi Arabia'
+// };
+//
+// function getLocation(location){
+//   if (location){
+//     return <p>location: {location}</p>;
+//   }
+// }
+//
+// let accountInformation = (
+// <div>
+// <h1>{userInformation.name ? userInformation.name : 'No name'}</h1>
+//     {userInformation.age >= 18 && <p>Age:{userInformation.age}</p>}
+// {getLocation(userInformation.location)}
+// </div>
+// );
+//
+// let appRoot2 = document.getElementById("root");
+// ReactDOM.render(accountInformation, appRoot2);
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//         <p>This is my name</p>
+//     );
+//   }
+// }
+//
+// export default App;
---
+++ src/playground.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom'
+import React,  { Component } from 'react';
+import './App.css';
+
+var name = 'Mo';
+console.log('name', name);

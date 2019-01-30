defe102a1545a91f5ef450771e5af62e1e10d7a2
Mohammad Mahjoub <mmahjoub@westmont.edu>
2019-01-29 19:53:48 -0800

    Arrays in JSX

--- src/App.js
+++ src/App.js
@@ -3,38 +3,48 @@ import React,  { Component } from 'react';
 import './playground/playground'
 import './App.css';
 
-let count = 0;
-
-const addOne = () => {
-   count ++;
-   renterCounterApp();
-   console.log(count);
+const app = {
+    title: "This is my new title.",
+    subtitle: "This is my new subtitle.",
+    options: []
 };
 
-const minusOne = () => {
-   count --;
-   renterCounterApp();
-   console.log(count);
+const onFormSubmit = (e) => {
+    e.preventDefault(); // This will make sure that the page will not reload
+    const option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+    }
+    renderReactApp();
 };
 
-const reSetOne = () => {
-   count = 0;
-   renterCounterApp();
-   console.log(count);
+const removeFormSubmit = (e) => {
+     app.options = [];
+    renderReactApp();
 };
-
-const appBoot = document.getElementById('root2');
-
-const renterCounterApp = () => {
-    const template = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne} >+1</button>
-            <button onClick={minusOne} >-1</button>
-            <button onClick={reSetOne} >0</button>
-        </div>
+const numbers = [55, 12, 33];
+const appRoot = document.getElementById('root2');
+const renderReactApp = () => {
+const template = (
+   <div>
+        <h3>This is the indecision app</h3>
+        <h3>{app.title}</h3>
+        <h3>{app.subtitle}</h3>
+       {
+           app.options.map((option) => {
+               return <p key={option}>Option: {option}</p>
+               })
+       }
+       <form onSubmit={onFormSubmit}>
+            <input type='text' name='option'/>
+           <button>Add option</button>
+       </form>
+       <button onClick={removeFormSubmit}>Remove all</button>
+   </div>
 );
-ReactDOM.render(template, appBoot);
+
+ReactDOM.render(template, appRoot);
 };
 
-renterCounterApp();
+renderReactApp();

7f8160fce44709303c12e48c44af3ca90d13bbad
Mohammad Mahjoub <mmahjoub@westmont.edu>
2019-01-29 20:23:49 -0800

    The toggle chanllage.

--- src/App.js
+++ src/App.js
@@ -3,41 +3,38 @@ import React,  { Component } from 'react';
 import './playground/playground'
 import './App.css';
 
-const app = {
-    title: "This is my new title.",
-    subtitle: "This is my new subtitle.",
-    options: []
-};
 
-const onFormSubmit = (e) => {
-    e.preventDefault(); // This will make sure that the page will not reload
-    const option = e.target.elements.option.value;
-    if (option) {
-        app.options.push(option);
-        e.target.elements.option.value = '';
-    }
-    renderReactApp();
-};
 
-const removeFormSubmit = (e) => {
-     app.options = [];
-    renderReactApp();
-};
 const appRoot = document.getElementById('root2');
+let toggle = false;
+
+function updateValue() {
+    toggle = !toggle;
+    renderReactApp();
+}
+
+const showInformation = () => {
+    if (toggle){
+        return (
+            <div>
+                <button onClick={updateValue}>Hide information</button>
+                 <h3>Hello world</h3>
+            </div>
+        );
+    }else {
+         return (
+            <div>
+                <button onClick={updateValue}>Show information</button>
+            </div>
+        );
+    }
+};
+
 const renderReactApp = () => {
 const template = (
    <div>
-        <h3>This is the indecision app</h3>
-        <h3>{app.title}</h3>
-        <h3>{app.subtitle}</h3>
-       {
-           app.options.map((option) => <p key={option}>Option: {option}</p>)
-       }
-       <form onSubmit={onFormSubmit}>
-            <input type='text' name='option'/>
-           <button>Add option</button>
-       </form>
-       <button onClick={removeFormSubmit}>Remove all</button>
+        <h3>Toggle App</h3>
+        {showInformation()}
    </div>
 );
 

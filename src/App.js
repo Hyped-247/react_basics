5d49b73c1876076bcd029df92d9cb96812ca0fe0
Mohammad Mahjoub <mmahjoub@westmont.edu>
2019-02-07 15:10:41 -0800

    Now we have counter ex working.

--- src/App.js
+++ src/App.js
@@ -3,42 +3,54 @@ import React,  { Component } from 'react';
 import './playground/playground'
 import './App.css';
 
+class Counter extends Component{
 
+    constructor(props){
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count: 0
+        };
+    }
 
-const appRoot = document.getElementById('root2');
-let toggle = false;
+    handleAddOne(event){
+        this.setState((preState) => {
+                return {
+                    count: preState.count + 1
+                };
+            }
+        );
+    };
 
-function updateValue() {
-    toggle = !toggle;
-    renderReactApp();
-}
+    handleMinusOne(event){
+        this.setState((preState) => {
+            return {
+                count: preState.count - 1
+            };
+        });
+    };
 
-const showInformation = () => {
-    if (toggle){
+    handleReset(event){
+           this.setState(() => {
+            return {
+                count: 0
+            };
+        });
+    };
+
+    render() {
         return (
             <div>
-                <button onClick={updateValue}>Hide information</button>
-                 <h3>Hello world</h3>
-            </div>
-        );
-    }else {
-         return (
-            <div>
-                <button onClick={updateValue}>Show information</button>
+                <h1>Count: {this.state.count} </h1>
+                <button onClick={this.handleAddOne}>+1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>reset</button>
             </div>
         );
     }
-};
 
-const renderReactApp = () => {
-const template = (
-   <div>
-        <h3>Toggle App</h3>
-        {showInformation()}
-   </div>
-);
+}
 
-ReactDOM.render(template, appRoot);
-};
-
-renderReactApp();
+ReactDOM.render(<Counter/>, document.getElementById('root'));
